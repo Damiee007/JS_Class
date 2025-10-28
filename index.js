531dec6639f53434bf84a2e741df053d4694087d
Damiee <alabie97@gmail.com.com>
2025-10-28 11:50:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// EXTERNAL JS
+function external(){
+    alert('SAMPLE: WELCOME TO JS CLASS 1')
+    alert('SAMPLE: WELCOME TO JS CLASS 2')
+    alert('SAMPLE: WELCOME TO JS CLASS 3')
+    alert('SAMPLE: WELCOME TO JS CLASS 4')
+    alert('SAMPLE: WELCOME TO JS CLASS 5')
+}
+
